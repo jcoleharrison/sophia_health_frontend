c8c7a3ec7f96b5f0f679c90d72a9127df30e18e3
Steven Lim <stevenylim17@gmail.com>
2024-01-22 12:20:12 -0600

    sidebar items fully updated and added to sidebar component for easier readability

--- components/side-bar-item.tsx
+++ components/side-bar-item.tsx
@@ -1,29 +1,15 @@
-import React from "react"
+import React, { Component } from "react"
 
 
 interface SidebarItemProps {
     name: string;
-    image: string;
+    image: React.ReactNode;
 }
 export default function SidebarItem (props:SidebarItemProps) {
     return (
         <a>
           <div className="flex cursor-pointer items-center rounded-sm px-3 py-3 duration-300 hover:bg-slate-100">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="lucide lucide-home"
-            >
-              <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-              <polyline points="9 22 9 12 15 12 15 22" />
-            </svg>
+            {props.image}
             <span className="px-3">{props.name}</span>
           </div>
         </a>
--- components/side-bar.tsx
+++ components/side-bar.tsx
@@ -6,6 +6,7 @@ import {
   AccordionTrigger,
 } from '@/components/accordion'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import { Home, Users, GanttChartSquare, Settings, BarChart3, MessageSquareDot, MessageCircleQuestion, LogOut} from 'lucide-react'
 import SidebarItem from './side-bar-item'
 
 export default function Sidebar() {
@@ -23,50 +24,9 @@ export default function Sidebar() {
           </Avatar>
           <span className="px-3">{name}</span>
         </div>
-
-        <a>
-          <div className="flex cursor-pointer items-center rounded-sm px-3 py-3 duration-300 hover:bg-slate-100">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="lucide lucide-home"
-            >
-              <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-              <polyline points="9 22 9 12 15 12 15 22" />
-            </svg>
-            <span className="px-3">Home</span>
-          </div>
-        </a>
-        <a>
-          <div className="flex cursor-pointer items-center rounded-sm px-3 py-3 duration-300 hover:bg-slate-100">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="lucide lucide-users"
-            >
-              <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-              <circle cx="9" cy="7" r="4" />
-              <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
-              <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-            </svg>
-            <span className="px-3">Patients</span>
-          </div>
-        </a>
-
+        
+        <SidebarItem name="Home" image={<Home />} />
+        <SidebarItem name="Patients" image={<Users />} />
         <Accordion
           type="single"
           collapsible
@@ -105,113 +65,12 @@ export default function Sidebar() {
             </AccordionContent>
           </AccordionItem>
         </Accordion>
-        <a>
-          <div className="flex cursor-pointer items-center rounded-sm px-3 py-3 duration-300 hover:bg-slate-100">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="lucide lucide-settings"
-            >
-              <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
-              <circle cx="12" cy="12" r="3" />
-            </svg>
-            <span className="px-3">Settings</span>
-          </div>
-        </a>
-        <a>
-          <div className="flex cursor-pointer items-center rounded-sm px-3 py-3 duration-300 hover:bg-slate-100">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="lucide lucide-bar-chart-3"
-            >
-              <path d="M3 3v18h18" />
-              <path d="M18 17V9" />
-              <path d="M13 17V5" />
-              <path d="M8 17v-3" />
-            </svg>
-            <span className="px-3">Reports/Analytics</span>
-          </div>
-        </a>
-        <a>
-          <div className="flex cursor-pointer items-center rounded-sm px-3 py-3 duration-300 hover:bg-slate-100">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="lucide lucide-message-square-dot"
-            >
-              <path d="M11.7 3H5a2 2 0 0 0-2 2v16l4-4h12a2 2 0 0 0 2-2v-2.7" />
-              <circle cx="18" cy="6" r="3" />
-            </svg>
-            <span className="px-3">Messages/Notifications</span>
-          </div>
-        </a>
-        <a>
-          <div className="flex cursor-pointer items-center rounded-sm px-3 py-3 duration-300 hover:bg-slate-100">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="lucide lucide-message-circle-question"
-            >
-              <path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z" />
-              <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
-              <path d="M12 17h.01" />
-            </svg>
-            <span className="px-3">Support/Help</span>
-          </div>
-        </a>
-
-        <a>
-          <div className="flex cursor-pointer items-center rounded-sm px-3 py-3 duration-300 hover:bg-slate-100">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="lucide lucide-log-out"
-            >
-              <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
-              <polyline points="16 17 21 12 16 7" />
-              <line x1="21" x2="9" y1="12" y2="12" />
-            </svg>
-            <span className="px-3">Sign Out</span>
-          </div>
-        </a>
+        <SidebarItem name="Settings" image={<Settings />} />
+        <SidebarItem name="Reports/Analytics" image={<BarChart3 />} />
+        <SidebarItem name="Messages/Notifications" image={<MessageSquareDot />} />
+        <SidebarItem name="Support/Help" image={<MessageCircleQuestion />} />
+        <SidebarItem name="Sign Out" image={<LogOut />} />
       </ScrollArea>
-      <h1>Hello World</h1>
     </main>
   )
 }
