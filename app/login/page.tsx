116204ad969e3d779f1763664445f91052f25720
Steven Lim <stevenylim17@gmail.com>
2024-01-27 23:17:43 -0600

    tested out custom functionality with sign in/sign out buttons with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,25 @@
-import { currentUser } from '@clerk/nextjs'
+'use client'
 
-export default async function Home() {
-  const user = await currentUser()
+import UserAvatarProfile from '@/components/user-avatar-profile'
+import {
+  UserButton,
+  UserProfile,
+  currentUser,
+  useClerk,
+  useUser,
+} from '@clerk/nextjs'
+
+export default function Home() {
+  const { user } = useUser()
+  const clerk = useClerk()
   console.log(user)
   return (
     <div>
       <div>
+        <UserButton />
         Hello {user?.firstName} {user?.lastName}
+        <UserAvatarProfile />
+        <button onClick={() => clerk.openSignIn({})}>Sign In Button</button>
       </div>
     </div>
   )
---
+++ components/user-avatar-profile.tsx
@@ -0,0 +1,82 @@
+'use client'
+
+import { cva, type VariantProps } from 'class-variance-authority'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import { cn } from '@/lib/utils'
+import { Skeleton } from './ui/skeleton'
+import { useState } from 'react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+import {
+  currentUser,
+  SignIn,
+  SignInButton,
+  SignOutButton,
+  useClerk,
+  useUser,
+  useSignUp,
+} from '@clerk/nextjs'
+
+const avatarSizes = cva('', {
+  variants: {
+    size: {
+      default: 'h-8 w-8',
+      lg: 'h-14 w-14',
+    },
+  },
+  defaultVariants: {
+    size: 'default',
+  },
+})
+
+interface UserAvatarProps extends VariantProps<typeof avatarSizes> {
+  username: string
+  imageUrl: string
+  isLive?: boolean
+  showBadge?: boolean
+}
+
+interface UserAvatarSkeletonProps extends VariantProps<typeof avatarSizes> {}
+
+export const UserAvatarSkeleton = ({ size }: UserAvatarSkeletonProps) => {
+  return <Skeleton className={cn('rounded-full', avatarSizes({ size }))} />
+}
+
+export default function UserAvatarProfile() {
+  const clerk = useClerk()
+  const { user } = useUser()
+  console.log(user?.firstName)
+  return (
+    <div className="right-0 mr-4">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar>
+            <AvatarImage className="object-cover" src={user?.imageUrl} />
+            <AvatarFallback>
+              {user?.firstName?.charAt(0)}
+              {user?.lastName?.charAt(0)}
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>
+            {user?.firstName} {user?.lastName}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>
+            <button onClick={() => clerk.signOut({})}>Sign Out</button>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  )
+}
