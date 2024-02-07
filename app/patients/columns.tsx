5582f7cb2caebc901f731820cc342450bc068627
Steven Lim <stevenylim17@gmail.com>
2024-02-06 19:43:26 -0600

    created columns, data-table, and page for patients. Table and sort functionality

---
+++ app/patients/columns.tsx
@@ -0,0 +1,94 @@
+'use client'
+import { Button } from '@/components/ui/button'
+// columns.tsx (client component) will contain our column definitions.
+import { ColumnDef } from '@tanstack/react-table'
+import { ArrowUpDown, MoreHorizontal } from 'lucide-react'
+
+//This type is used t odefine the shape of our data
+//You can use a Zod schema here if you want
+
+export type Patient = {
+  name: string
+  id: string
+  lastSession: Date
+  criticalNotes: string
+  status: 'Active' | 'Inactive'
+}
+
+export const columns: ColumnDef<Patient>[] = [
+  {
+    accessorKey: 'name',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className="p-0"
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+  {
+    accessorKey: 'id',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className="p-0"
+        >
+          ID
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+  {
+    accessorKey: 'lastSession',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className="p-0"
+        >
+          Last Session
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+  {
+    accessorKey: 'criticalNotes',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className="p-0"
+        >
+          Critical Notes
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+  {
+    accessorKey: 'status',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className="p-0"
+        >
+          Status
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+]
--- app/patients/page.tsx
+++ app/patients/page.tsx
@@ -1,3 +1,102 @@
-export default function Home() {
-  return <div>Patients Page Placeholder</div>
+// page.tsx (server component) is where we'll fetch data and render our table.
+import { Patient, columns } from './columns'
+import { DataTable } from './data-table'
+async function getData(): Promise<Patient[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      name: 'Cole Harrison',
+      id: '1',
+      lastSession: new Date('2/6/24 07:08'),
+      criticalNotes: 'Nervous Ticking',
+      status: 'Active',
+    },
+    {
+      name: 'Steven Lim',
+      id: '2',
+      lastSession: new Date('1/30/24 07:08'),
+      criticalNotes: 'Nail Biting',
+      status: 'Inactive',
+    },
+    {
+      name: 'Austin Davis',
+      id: '3',
+      lastSession: new Date('1/23/24 07:08'),
+      criticalNotes: 'General Questions',
+      status: 'Active',
+    },
+    {
+      name: 'LeBron James',
+      id: '4',
+      lastSession: new Date('1/16/24 07:08'),
+      criticalNotes: 'Likes Dunking',
+      status: 'Active',
+    },
+    {
+      name: 'Kevin Durant',
+      id: '5',
+      lastSession: new Date('4/6/22 07:08'),
+      criticalNotes: 'Likes Shooting',
+      status: 'Inactive',
+    },
+    {
+      name: 'Austin Reeves',
+      id: '6',
+      lastSession: new Date('5/14/21 07:08'),
+      criticalNotes: 'Oklahoma',
+      status: 'Active',
+    },
+    {
+      name: 'Rajon Rondo',
+      id: '7',
+      lastSession: new Date('12/9/23 07:08'),
+      criticalNotes: 'Pass-first',
+      status: 'Active',
+    },
+    {
+      name: 'George Lopez',
+      id: '8',
+      lastSession: new Date('12/2/23 07:08'),
+      criticalNotes: 'Comedian',
+      status: 'Inactive',
+    },
+    {
+      name: 'Baron Davis',
+      id: '9',
+      lastSession: new Date('9/18/22 07:08'),
+      criticalNotes: 'Shooter/Scorer',
+      status: 'Active',
+    },
+    {
+      name: 'Steph Curry',
+      id: '10',
+      lastSession: new Date('5/24/21 07:08'),
+      criticalNotes: '3 Point Specialist',
+      status: 'Active',
+    },
+    {
+      name: 'Robert Darwin',
+      id: '11',
+      lastSession: new Date('12/6/22 07:08'),
+      criticalNotes: 'Entrepreneur',
+      status: 'Inactive',
+    },
+    {
+      name: 'Anthony Davis',
+      id: '12',
+      lastSession: new Date('12/12/20 07:08'),
+      criticalNotes: 'University of Kentucky',
+      status: 'Active',
+    },
+  ]
+}
+
+export default async function Home() {
+  const data = await getData()
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
 }
